975c28d039ed91bde0c721f827e4fe0a6902e6b5
CesarTun <ctuncetina@gmail.com>
2026-08-26 10:32:28 -0600

    chore: basic routes setted

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
+import 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -37,14 +38,19 @@ class _CustomListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    
+
     return ListTile(
-      leading: Icon(menuItem.icon, color: colors.primary,),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        //TODO: navigate to other screen
+        //Recommended way to apply navigation in flutter for web compatibility
+        // Navigator.of(context).push(
+        //   MaterialPageRoute<void>(builder: (context) => const ButtonsScreen()),
+        // );
+        //Named routing
+        Navigator.pushNamed(context, menuItem.url);
       },
     );
   }
