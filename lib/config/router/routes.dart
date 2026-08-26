d1d6e3c819e5e8a62c317a3169927c0f0bfb2918
CesarTun <ctuncetina@gmail.com>
2026-08-26 16:03:52 -0600

    chore: new screens created and rutes master and barrel updated

--- lib/config/router/routes.dart
+++ lib/config/router/routes.dart
@@ -1,6 +1,11 @@
-
 class Routes {
   static const String homeScreen = '/';
   static const String cardsScreen = '/cards';
   static const String buttonsScreen = '/buttons';
+  static const String animatedScreen = '/animated';
+  static const String appTutorialScreen = '/app_tutorial';
+  static const String infiniteScrollScreen = '/infinite_scroll';
+  static const String snackBarScreen = '/snackbar';
+  static const String uiControlsScreen = '/ui_controls';
+  static const String progress = '/progress';
 }
